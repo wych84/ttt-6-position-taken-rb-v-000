260629805c5549bd772b2f66837df0e3a965a21e
Joyce Chun <wooyangee@gmail.com>
2018-04-10 23:10:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index, character="X")
   board[index] = character
-  if board == " "
-    false
-  elsif board == "X"
+  if board == "X"
     true
+  elsif board == " "
+    false
   else
     false
   end  
